e78ce9374bef6f3106925dcb15a77f0638a8a053
emrah <emrahk@outlook.com>
2021-12-14 14:29:00 +0100

    added pwsh profile

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,40 @@
+# Modules
+## Import oh-my-posh
+oh-my-posh --init --shell pwsh --config C:\Users\emrah.kaya\.mytheme.omp.json | Invoke-Expression
+
+## Get fancy directory listing
+Import-Module -Name Terminal-Icons
+
+## Add git autocomplete
+Import-Module posh-git
+
+## Add history autocomplete
+if ($host.Name -eq 'ConsoleHost')
+{
+  Import-Module PSReadLine
+  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+  Set-PSReadLineOption -PredictionViewStyle InlineView #ListView
+  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+  Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+  Set-PSReadLineOption -ShowToolTips
+  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+}
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+  param($commandName, $wordToComplete, $cursorPosition)
+  dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+
+# Aliases & functions
+## Git
+function gitwrecked { git reset --hard HEAD }
+function gl { git log --oneline --decorate --color --graph $args }
+function glo { git log --oneline --decorate --color $args }
+function gs { git status }
+
+## Bash-like commands
+function dl { Get-ChildItem $args | Format-Wide -column 3 }
